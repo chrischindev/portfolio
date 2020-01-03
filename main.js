ba70eab9ddf5dc79d3252c429fc76aa640c3894d
Chris Chin <chin.christopher@gmail.com>
2020-01-02 19:38:57 -0500

    update side navbar to be more responsive

--- main.js
+++ main.js
@@ -1,24 +1,41 @@
 const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li");
+  const navLinksLi = document.querySelectorAll(".nav-links li");
+  const navLinksA = document.querySelectorAll(".nav-links a");
 
   burger.addEventListener("click", () => {
     // Toggle Nav
     nav.classList.toggle("nav-active");
 
     // Animate Links
-    navLinks.forEach((link, index) => {
+    navLinksLi.forEach((link, index) => {
       if (link.style.animation) {
         link.style.animation = "";
       } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 10 +
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 15 +
           0.5}s`;
       }
     });
     // Burger Animation
     burger.classList.toggle("toggle");
   });
+
+  navLinksA.forEach(link =>
+    link.addEventListener("click", () => {
+      if (nav.classList.contains("nav-active")) {
+        // Close Navbar when Anchor tags are clicked
+        nav.classList.remove("nav-active");
+
+        burger.classList.toggle("toggle");
+
+        // Remove Style for each link
+        navLinksLi.forEach(link => {
+          link.style.animation = "";
+        });
+      }
+    })
+  );
 };
 
 navSlide();
