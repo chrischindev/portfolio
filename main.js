3d161508c1375d6555e8b7f3988ffe0eaba9a951
Chris Chin <chin.christopher@gmail.com>
2020-01-08 02:46:18 -0500

    add styling for mobile devices

--- main.js
+++ main.js
@@ -88,51 +88,6 @@ function showSlides(n, no) {
   dots[slideIndex[no] - 1].className += " active";
 }
 
-// const logo = document.getElementById("logo");
-// const linkAboutMe = document.getElementById("linkAboutMe");
-// const linkProjects = document.getElementById("linkProjects");
-// const linkResume = document.getElementById("linkResume");
-// const linkContact = document.getElementById("linkContact");
-
-// const linkRecipix = document.getElementById("linkRecipix");
-// const linkRaceHopper = document.getElementById("linkRaceHopper");
-// const linkBotBuddies = document.getElementById("linkBotBuddies");
-
-const hero = document.getElementById("top");
-const aboutMe = document.getElementById("aboutMe");
-const projects = document.getElementById("projects");
-const recipix = document.getElementById("recipix");
-const racehopper = document.getElementById("racehopper");
-const botbuddies = document.getElementById("botbuddies");
-const resume = document.getElementById("resume");
-const contact = document.getElementById("contact");
-
-// logo.addEventListener("click", () =>
-//   hero.scrollIntoView({ behavior: "smooth" })
-// );
-// linkAboutMe.addEventListener("click", () =>
-//   aboutMe.scrollIntoView({ behavior: "smooth" })
-// );
-
-// linkProjects.addEventListener("click", () =>
-//   projects.scrollIntoView({ behavior: "smooth" })
-// );
-// linkResume.addEventListener("click", () =>
-//   resume.scrollIntoView({ behavior: "smooth" })
-// );
-// linkContact.addEventListener("click", () =>
-//   contact.scrollIntoView({ behavior: "smooth" })
-// );
-// linkRecipix.addEventListener("click", () => {
-//   recipix.scrollIntoView({ behavior: "smooth" });
-// });
-// linkRaceHopper.addEventListener("click", () =>
-//   racehopper.scrollIntoView({ behavior: "smooth" })
-// );
-// linkBotBuddies.addEventListener("click", () =>
-//   botbuddies.scrollIntoView({ behavior: "smooth" })
-// );
-
 let date = new Date();
 let year = date.getFullYear();
 let yearSpan = document.getElementById("year");
